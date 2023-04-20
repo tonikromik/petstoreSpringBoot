6d5ee28c45c27da3fd9798f6b5e3ef3c8c650f14
tonikromik <antonov.roman.o@gmail.com>
2023-04-20 12:16:29 +0300

    Added Test for all methods in UserServiceImpl

--- src/test/java/com/petstore/petstoreRest/service/UserServiceImplTest.java
+++ src/test/java/com/petstore/petstoreRest/service/UserServiceImplTest.java
@@ -14,6 +14,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +35,7 @@ public class UserServiceImplTest {
 
     private User user;
     private UserDTO userDTO;
+    private List<UserDTO> userDTOs;
 
     @BeforeEach
     public void init() {
@@ -40,6 +43,11 @@ public class UserServiceImplTest {
                 "email@gmail.com", "password", "+380970000001", 1);
         userDTO = new UserDTO(1L, "user", "first", "last",
                 "email@gmail.com", "password", "+380970000001", 1);
+        UserDTO userDTO2 = new UserDTO(2L, "user2", "first2", "last2",
+                "email2@gmail.com", "password2", "+380970000002", 1);
+        userDTOs = new ArrayList<>();
+        userDTOs.add(userDTO);
+        userDTOs.add(userDTO2);
     }
 
     @Test
@@ -54,7 +62,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void findByUsername_FailedTest() {
+    public void findByUsername_ThrowResponseStatusException() {
         when(userRepository.findByUserName(anyString())).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         assertThrows(ResponseStatusException.class, () -> userServiceImpl.findByUsername("user"));
@@ -75,11 +83,60 @@ public class UserServiceImplTest {
 
 
     @Test
-    public void saveUser_FailedTest() {
-        when(userRepository.save(any(User.class)))
-                .thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "User with such username already exist"));
-        when(userMapper.toEntity(any(UserDTO.class))).thenReturn(user);
+    public void saveUser_ThrowResponseStatusException() {
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.of(user));
 
         assertThrows(ResponseStatusException.class, () -> userServiceImpl.saveUser(userDTO));
+    }
+
+    @Test
+    public void updateUser_ReturnVoid(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.of(user));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        userServiceImpl.updateUser("user", userDTO);
+
+        verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    public void updateUser_ThrowResponseStatusException(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> userServiceImpl.updateUser(anyString(), userDTO));
+    }
+
+    @Test
+    public void deleteUser_ReturnVoid(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.of(user));
+        doNothing().when(userRepository).delete(user);
+
+        assertAll(()-> userServiceImpl.deleteUser("user"));
+        verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    public void deleteUser_ThrowResponseStatusException(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> userServiceImpl.deleteUser(anyString()));
+    }
+
+    @Test
+    public void saveAllUsers_ReturnVoid(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(userMapper.toEntity(any(UserDTO.class))).thenReturn(user);
+
+        userServiceImpl.saveAllUsers(userDTOs);
+
+        verify(userRepository, times(2)).save(any(User.class));
+    }
+
+    @Test
+    public void saveAllUsers_ThrowResponseStatusException(){
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.of(user));
+
+        assertThrows(ResponseStatusException.class, () -> userServiceImpl.saveAllUsers(userDTOs));
     }
 }
