a8a9a7562629a23529315731eec5ee144ed5b327
tonikromik <antonov.roman.o@gmail.com>
2023-06-07 17:56:31 +0300

    Added OpenApi description for security.

--- src/main/java/com/petstore/PetstoreApplication.java
+++ src/main/java/com/petstore/PetstoreApplication.java
@@ -1,7 +1,9 @@
 package com.petstore;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,16 @@ import java.nio.file.Paths;
 @OpenAPIDefinition(
         info = @Info(title = "Petstore API", version = "1.0.0", description = "This is a sample server Petstore server."),
         servers = @Server(url = "http://localhost:8080")
+
+)
+@SecurityScheme(
+        name = "BasicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic",
+        description = "HTTP Basic Authentication." +
+                "\nThere are two roles for testing application security: " +
+                "\nROLE_USER (login: user; password: user) and " +
+                "\nROLE_ADMIN (login: admin; password: admin)"
 )
 public class PetstoreApplication implements CommandLineRunner {
 
--- src/main/resources/db.datainit/V2_0_1__data_update_setting_roles_for_users.sql
+++ src/main/resources/db.datainit/V2_0_1__data_update_setting_roles_for_users.sql
@@ -1,2 +1,4 @@
 UPDATE users SET user_role = 'USER';
 UPDATE users SET user_role = 'ADMIN' WHERE id = 1;
+UPDATE users SET user_role = 'ADMIN' WHERE id = 3;
+
